0aad179eec6eae62ca4299ad5f4161fd4150cfa2
Federica-DeSimone <f.desimone51@studenti.unisa.it>
2026-06-01 13:29:54 +0200

    PasswordUtils

---
+++ src/main/java/it/unisa/model/PasswordUtil.java
@@ -0,0 +1,15 @@
+package it.unisa.model;
+
+import java.security.MessageDigest;
+
+public class PasswordUtil {
+    public static String hashPassword(String password) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hash = md.digest(password.getBytes("UTF-8"));
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            hexString.append(String.format("%02x", b));
+        }
+        return hexString.toString();
+    }
+}
