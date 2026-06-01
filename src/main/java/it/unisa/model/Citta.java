7dbdbf645d2180d4afe7f7e907338e4a727b7f36
Peppinator04 <giuseppetirelli74@gmail.com>
2026-06-01 13:24:48 +0200

    aggiunto file ciattà

---
+++ src/main/java/it/unisa/model/Citta.java
@@ -0,0 +1,15 @@
+package it.unisa.model;
+
+
+
+public class Citta {
+    private String nome;
+    private String regione;
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getRegione() { return regione; }
+    public void setRegione(String regione) { this.regione = regione; }
+}
+
