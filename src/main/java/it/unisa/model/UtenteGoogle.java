9f567f5df82a9626a4d39793cf4b8e02f1d1a9f2
Federica-DeSimone <f.desimone51@studenti.unisa.it>
2026-06-01 13:31:04 +0200

    UtenteGoogle.java

---
+++ src/main/java/it/unisa/model/UtenteGoogle.java
@@ -0,0 +1,22 @@
+package it.unisa.model;
+
+public class UtenteGoogle {
+    private int id;
+    private String nome;
+    private String email;
+    private String immagineProfilo;
+
+    // Getters e Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getImmagineProfilo() { return immagineProfilo; }
+    public void setImmagineProfilo(String immagineProfilo) { this.immagineProfilo = immagineProfilo; }
+}
+
